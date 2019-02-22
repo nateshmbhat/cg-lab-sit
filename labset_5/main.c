88c20c2542949a7a3e690b21adbcef1d5810bf9c
nateshmbhat <nateshmbhat1@gmail.com>
2019-02-22 13:29:42 +0530

    update labset 5

--- labset_5/main.c
+++ labset_5/main.c
@@ -6,37 +6,44 @@
 #define CMIN -3.0
 #define CMAX 3.0
 
+typedef struct Point{
+    double x ,y ;
+}Point ; 
 
 //global variables to keep track of 4 midpoint of a cell
-double m1x, m1y, m2x, m2y, m3x, m3y, m4x, m4y;
+Point m1 , m2 , m3 , m4 ; 
+const double dx =0.1 , dy = 0.1 ; //increase in x and y 
 
-//function to generate sample data
-int CreateSD(double x, double y)
+
+int CreateSampleData(double x, double y) //return the value of cassini's equation for the curve (sample data)
 {
     double a = 0.49, b = 0.5;
     return ceil((x * x + y * y + a * a) * (x * x + y * y + a * a) - 4 * a * a * x * x - b * b * b * b);
 }
 
-//function to identify a cell type
-int celltype(int a, int b, int c, int d)
+int celltype(int a, int b, int c, int d) //return the decimal value of the binary number dcba to identify cell type.
 {
     return a + b * 2 + c * 4 + d * 8;
 }
 
 void findmidpoints(float x, float y)
 {
-    //dx=dy=0.1
-    m1x = x;
-    m1y = y + 0.1 / 2.0;
+    m1.x = x;
+    m1.y = y + dy / 2.0;
 
-    m2x = x + 0.1 / 2.0;
-    m2y = y;
+    m2.x = x + dx / 2.0;
+    m2.y = y;
 
-    m3x = x + 0.1;
-    m3y = y + 0.1 / 2.0;
+    m3.x = x + dx ; 
+    m3.y = y + dy / 2.0;
 
-    m4x = x + 0.1 / 2.0;
-    m4y = y + 0.1;
+    m4.x = x + dx / 2.0;
+    m4.y = y + dy;
+}
+
+void drawLine(Point a , Point b){
+    glVertex2d(a.x, a.y) ; 
+    glVertex2d(b.x, b.y) ; 
 }
 
 //function to draw lines in a cell type
@@ -47,53 +54,29 @@ void drawLines(int n, float x, float y)
 
     switch (n)
     {
-    case 1:
-    case 14:
-        glVertex2d(m1x, m1y);
-        glVertex2d(m2x, m2y);
+    case 1: case 14:
+        drawLine(m1 , m2) ; break;
+    case 2: case 13:
+        drawLine(m2 , m3) ; break;
+    case 4: case 11:
+        drawLine(m3,m4) ; 
         break;
-    case 2:
-    case 13:
-        glVertex2d(m2x, m2y);
-        glVertex2d(m3x, m3y);
-        break;
-    case 4:
-    case 11:
-        glVertex2d(m4x, m4y);
-        glVertex2d(m3x, m3y);
-        break;
-    case 7:
-    case 8:
-        glVertex2d(m4x, m4y);
-        glVertex2d(m1x, m1y);
-        break;
-
-    case 3:
-    case 12:
-        glVertex2d(m1x, m1y);
-        glVertex2d(m3x, m3y);
-        break;
-    case 6:
-    case 9:
-        glVertex2d(m2x, m2y);
-        glVertex2d(m4x, m4y);
-        break;
-
+    case 7: case 8:
+        drawLine(m4 ,m1) ; break;
+    case 3: case 12:
+        drawLine(m1 ,m3) ; break;
+    case 6: case 9:
+        drawLine(m2 ,m4) ; break;
     case 5:
-        glVertex2d(m1x, m1y);
-        glVertex2d(m4x, m4y);
-        glVertex2d(m2x, m2y);
-        glVertex2d(m3x, m3y);
+        drawLine(m1 ,m4) ;
+        drawLine(m2 ,m3) ;
         break;
     case 10:
-        glVertex2d(m1x, m1y);
-        glVertex2d(m2x, m2y);
-        glVertex2d(m4x, m4y);
-        glVertex2d(m3x, m3y);
+        drawLine(m1 ,m2) ;
+        drawLine(m4 ,m3) ;
         break ; 
     }
     glEnd();
-    glFlush() ; 
 }
 
 void display()
@@ -101,23 +84,23 @@ void display()
     //array of sample data generated
     int SD[100][100];
     int i, j;
-    float x, y;
-    int c;
+    double x, y;
     glClear(GL_COLOR_BUFFER_BIT);
 
     //generate sample data and store on to an array
-    for (x = CMIN, i = 0; x < CMAX; i++, x += 0.1)
-        for (y = CMIN, j = 0; y < CMAX; j++, y += 0.1)
-            SD[i][j] = CreateSD(x, y);
+    for (x = CMIN, i = 0; x < CMAX; i++, x += dx)
+        for (y = CMIN, j = 0; y < CMAX; j++, y += dy)
+            SD[i][j] = CreateSampleData(x, y);
     //assign generated data to vertices in mesh and find cell type to draw lines
-    for (x = CMIN, i = 0; x < CMAX; i++, x += 0.1)
-        for (y = CMIN, j = 0; y < CMAX; j++, y += 0.1)
+    for (x = CMIN, i = 0; x < CMAX; i++, x += dx)
+        for (y = CMIN, j = 0; y < CMAX; j++, y += dy)
         {
-            c = celltype(SD[i][j], SD[i + 1][j], SD[i + 1][j + 1], SD[i][j + 1]);
-            drawLines(c, x, y);
+            int ct = celltype(SD[i][j], SD[i + 1][j], SD[i + 1][j + 1], SD[i][j + 1]);
+            drawLines(ct, x, y);
         }
     glFlush();
 }
+
 void myReshape(int w, int h)
 {
     glViewport(0, 0, w, h);
@@ -131,7 +114,6 @@ void myReshape(int w, int h)
 }
 
 int main(int argc, char **argv)
-
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
@@ -141,5 +123,4 @@ int main(int argc, char **argv)
     glClearColor(0.0, 0.0, 0.0, 0.0);
     glColor3f(1.0, 1.0, 1.0);
     glutMainLoop();
-    return 0;
 }
