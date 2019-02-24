924e79b817252ecd004649ba4d1877bbbe941cbd
nateshmbhat <nateshmbhat1@gmail.com>
2019-02-24 13:50:26 +0530

    update labest 2

--- labset_2/main.c
+++ labset_2/main.c
@@ -1,92 +1,98 @@
 #include <stdio.h>
 #include <GL/glut.h>
-void display2D()
+
+void display2d()
 {
     glClear(GL_COLOR_BUFFER_BIT);
-    glClearColor(1.0, 0.0, 0.0, 0.0);
-    glBegin(GL_POLYGON);
-    glVertex2f(0.0, 0.0);
-    glVertex2f(0.8, 0.2);
-    glVertex2f(0.5, 0.5);
-    glVertex2f(0.8, 0.7);
 
-    glEnd();
+    glColor3f(1,0,0) ; 
+
+    glRectd(-1 , -1 ,1 , 1) ; 
+
     glFlush();
 }
-void display3D()
+
+void display3d()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glClearColor(1.0, 0.0, 0.0, 0.0);
-    glBegin(GL_POLYGON);
-    glVertex2f(0.0, 0.0);
-    glVertex2f(0.8, 0.2);
-    glVertex2f(0.5, 0.5);
-    glVertex2f(0.8, 0.7);
+    glClearColor(1,1,1,1) ; 
+    glColor3f(1,0,0) ; 
 
-    glEnd();
+    glPushMatrix() ; 
+    glutWireTeapot(1) ; 
+    glPopMatrix() ; 
+
     glFlush();
 }
-void init2D(int ch)
+
+
+void init2d(int ch)
 {
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     if (ch == 1)
-        glutCreateWindow("2D Orthogonal Projection");
+        glutCreateWindow("2d orthogonal view");
     else
-        glutCreateWindow("2D Perspective Projection");
-    glutDisplayFunc(display2D);
+        glutCreateWindow("2d perspective view");
+    glutDisplayFunc(display2d);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     if (ch == 1)
         gluOrtho2D(-2.0, 2.0, -2.0, 2.0);
-
     else
-        gluPerspective(90.0, 2.0, 0.0, 0.0);
+    {
+        gluPerspective(30.0, 1.0, 1.0, 100.0);
+        gluLookAt(3.0, 3.0, 3.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    }
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
+    glutMainLoop();
 }
-
-void init3D(int ch)
+void init3d(int ch)
 {
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
     if (ch == 1)
-        glutCreateWindow("3D Orthogonal Projection");
+        glutCreateWindow("3d orthogonal view");
     else
-        glutCreateWindow("3D Perspective Projection");
-    glutDisplayFunc(display3D);
+        glutCreateWindow("3d perspective view");
+    glutDisplayFunc(display3d);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-
     if (ch == 1)
         glOrtho(-2.0, 2.0, -2.0, 2.0, -2.0, 2.0);
-
     else
-        glFrustum(-2.0, 2.0, -2.0, 2.0, -2.0, 2.0);
+    {
+        glFrustum(-2, 2, -2, 2, 1, 10);
+        gluLookAt(1, 2, 2, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    }
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    glEnable(GL_DEPTH_TEST);
+    glutMainLoop();
 }
-
 
 int main(int argc, char **argv)
 {
-    int ch;
+    int ch ; 
     glutInit(&argc, argv);
-    printf("Enter the choice: \n 1: 2D orthogoal Projection\n 2: 2D Perspective Projection\n 3: 3D orthogoal Projection\n 4: 3D Perspective Projection\n");
-    scanf("%d", &ch);
-    switch (ch)
+    while (1)
     {
-    case 1:
-        init2D(1);
-        break;
-    case 2:
-        init2D(2);
-        break;
-    case 3:
-        init3D(1);
-        break;
-    case 4:
-        init3D(2);
+        printf("\nenter your choice:\n1.2d with ortho\n2.2d with perspective\n3.3d with ortho\n4.3d with perspective\n");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+            case 1:
+                init2d(1);
+                break;
+            case 2:
+                init2d(2);
+                break;
+            case 3:
+                init3d(1);
+                break;
+            case 4:
+                init3d(2);
+                break;
+        }
+        glEnable(GL_DEPTH_TEST);
     }
-    glutMainLoop();
     return 0;
 }
