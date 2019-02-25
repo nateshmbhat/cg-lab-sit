37faa2ba41416726a27d727d29cace26d9d3341d
nateshmbhat <nateshmbhat1@gmail.com>
2019-02-25 23:05:23 +0530

    update

--- testing/main.cpp
+++ testing/main.cpp
@@ -2,28 +2,56 @@
 #include<GL/glut.h>
 using namespace std ; 
 
+typedef struct{
+	float x , y ; 
+}Point ; 
+
+Point m1 ,m2 , m3 ,m4 ; 
+float dx = 0.1 ; 
+
+int cellType(int a , int b , int c , int d){
+	return a+ b*2 + c*4 + d*8 ; 
+}
+
+void findMidPoints(float x , float y){
+	m1.y = y+dx/2 ; 	
+	m1.x = x ; 
+
+	m2.y = y+dx ; 
+	m2.x = x+dx/2 ; 
+
+	m3.x = x+dx ; 
+	m3.y = y+dx/2 ; 
+
+	m4.x = x+dx/2 ; 
+	m4.y = y ; 
+}
+
+void displayLines(int cell , float x , float y ){
+	findMidPoints(x,y) ; 
+	switch(cell){
+
+	}
+}
+
+float createSD(float x , float y){
+	float a= 0.49 , b= 0.5 ; 
+}
 
 void display(){
     glClear(GL_COLOR_BUFFER_BIT) ; 
+	glClearColor(1,1,1,1) ; 
     glColor3f(1 , 0 , 0) ; 
 
-    glBegin(GL_POLYGON)  ; 
-        glVertex2f(100 , 0) ; 
-        glVertex2f(108 , 54) ; 
-        glVertex2f(200 , 38) ; 
-        glVertex2f( 20 , 28) ; 
-    glEnd() ; 
+
+
 
     glFlush() ; 
 }
 
 void reshape(int w, int h ){
-	glViewport(0 , 0 , w , h) ; 
-	glMatrixMode(GL_PROJECTION) ; 
-	glLoadIdentity() ; 
-	gluOrtho2D(0 , w , 0 , h) ; 
-	glMatrixMode(GL_MODELVIEW) ; 
-	glLoadIdentity() ; 
+	glViewport(0,0,w,h) ; 
+	glutPostRedisplay() ; 
 }
 
 
